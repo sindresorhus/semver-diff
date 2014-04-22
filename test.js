9665090f957b1181d0ca54cafa3c1c551a5f7264
Sindre Sorhus <sindresorhus@gmail.com>
2014-04-22 02:15:11 +0200

    init

---
+++ test.js
@@ -0,0 +1,13 @@
+'use strict';
+var assert = require('assert');
+var semverDiff = require('./index');
+
+it('should get the semver diff type', function () {
+	assert.strictEqual(semverDiff('0.0.1', '1.0.0'), 'major');
+	assert.strictEqual(semverDiff('0.0.1', '0.1.0'), 'minor');
+	assert.strictEqual(semverDiff('0.0.1', '0.0.2'), 'patch');
+	assert.strictEqual(semverDiff('0.0.1-foo', '0.0.1-foo.bar'), 'prerelease');
+	assert.strictEqual(semverDiff('0.0.1', '0.0.1+foo.bar'), 'build');
+	assert.strictEqual(semverDiff('0.0.1', '0.0.1'), null);
+	assert.strictEqual(semverDiff('0.0.2', '0.0.1'), null);
+});
