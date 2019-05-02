ac7914f26fdbc5f150583be97c37d308c7552651
Corey Farrell <git@cfware.com>
2019-05-02 08:00:12 -0400

    Return undefined if the second version has an older build
    Fixes #5

--- test.js
+++ test.js
@@ -7,7 +7,16 @@ test('should get the semver diff type', t => {
 	t.is(semverDiff('0.0.1', '0.1.0'), 'minor');
 	t.is(semverDiff('0.0.1', '0.0.2'), 'patch');
 	t.is(semverDiff('0.0.1-foo', '0.0.1-foo.bar'), 'prerelease');
-	t.is(semverDiff('0.0.1', '0.0.1+foo.bar'), 'build');
 	t.is(semverDiff('0.0.1', '0.0.1'), undefined);
 	t.is(semverDiff('0.0.2', '0.0.1'), undefined);
+
+	t.is(semverDiff('0.0.1', '0.0.1+foo.bar'), 'build');
+	t.is(semverDiff('0.0.1+0', '0.0.1'), undefined);
+	t.is(semverDiff('0.0.1+2', '0.0.1+2'), undefined);
+	t.is(semverDiff('0.0.1+3', '0.0.1+2'), undefined);
+	t.is(semverDiff('0.0.1+1', '0.0.1+2'), 'build');
+	t.is(semverDiff('0.0.1+2', '0.0.1+2.0'), 'build');
+	t.is(semverDiff('0.0.1+2.0', '0.0.1+2'), undefined);
+	t.is(semverDiff('0.0.1+2.a', '0.0.1+2.0'), undefined);
+	t.is(semverDiff('0.0.1+2.0', '0.0.1+2.a'), 'build');
 });
