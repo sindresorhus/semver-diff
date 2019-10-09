a4d76827eb158efb369bd1933e1faedac2149721
Aden Narkowicz <aden@dcnstrct.com>
2019-10-09 13:21:02 +1100

    Add possible return types 'premajor', 'preminor' and 'prepatch'
    Include missing return types 'premajor', 'preminor' and 'prepatch' in tests, TS types and examples.

--- test.js
+++ test.js
@@ -6,6 +6,9 @@ test('should get the semver diff type', t => {
 	t.is(semverDiff('0.0.1', '1.0.0'), 'major');
 	t.is(semverDiff('0.0.1', '0.1.0'), 'minor');
 	t.is(semverDiff('0.0.1', '0.0.2'), 'patch');
+	t.is(semverDiff('0.0.1-foo', '1.0.0'), 'premajor');
+	t.is(semverDiff('0.0.1-foo', '0.1.0'), 'preminor');
+	t.is(semverDiff('1.1.1-foo', '1.1.2'), 'prepatch');
 	t.is(semverDiff('0.0.1-foo', '0.0.1-foo.bar'), 'prerelease');
 	t.is(semverDiff('0.0.1', '0.0.1'), undefined);
 	t.is(semverDiff('0.0.2', '0.0.1'), undefined);
